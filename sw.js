b5450fc9ba4417baaca0ded426a2c1c134aecb39
GreeneyedGirl99 <melindabunner@gmail.com>
2026-09-17 00:50:26 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = const CACHE = 'geg-v5-cache-v1';
+const CACHE = 'geg-v6-cache-v1';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
